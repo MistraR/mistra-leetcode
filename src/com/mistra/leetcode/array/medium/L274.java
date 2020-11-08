d85fe02d5ad0626ece250b59592a76591d2d183b
MistraR <842404548@qq.com>
2020-11-08 23:01:26 +0800

    274

---
+++ src/com/mistra/leetcode/array/medium/L274.java
@@ -0,0 +1,22 @@
+package com.mistra.leetcode.array.medium;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2020/11/8 10:59 下午
+ * @ Description:
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+public class L274 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{3,0,6,1,5};
+        System.out.println(hIndex(arr));
+    }
+
+    public static int hIndex(int[] citations) {
+
+    }
+}
