5b11dab57132cd79906808ed87c0387338922aa9
MistraR <842404548@qq.com>
2020-11-09 21:59:06 +0800

    274

--- src/com/mistra/leetcode/array/medium/L274.java
+++ src/com/mistra/leetcode/array/medium/L274.java
@@ -1,5 +1,7 @@
 package com.mistra.leetcode.array.medium;
 
+import java.util.Arrays;
+
 /**
  * @author Mistra
  * @ Version: 1.0
@@ -12,11 +14,18 @@ package com.mistra.leetcode.array.medium;
 public class L274 {
 
     public static void main(String[] args) {
-        int[] arr = new int[]{3,0,6,1,5};
+        int[] arr = new int[]{3, 0, 6, 1, 5};
         System.out.println(hIndex(arr));
     }
 
     public static int hIndex(int[] citations) {
-
+        Arrays.sort(citations);
+        int i = 0;
+        for (int i1 = citations.length - 1; i1 < citations.length && i1 > -1; i1--) {
+            if (citations[i1] > i) {
+                i++;
+            }
+        }
+        return i;
     }
 }
