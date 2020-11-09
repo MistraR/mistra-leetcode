d334e0eae20ad727853fc6b1aa01f5cabd3b791e
MistraR <842404548@qq.com>
2020-11-09 22:34:15 +0800

    453

--- src/com/mistra/leetcode/array/easy/L453.java
+++ src/com/mistra/leetcode/array/easy/L453.java
@@ -5,6 +5,8 @@ package com.mistra.leetcode.array.easy;
  * @ Version: 1.0
  * @ Time: 2020/11/9 9:59 下午
  * @ Description:
+ * 给定一个长度为 n 的非空整数数组，找到让数组所有元素相等的最小移动次数。每次移动将会使 n - 1 个元素增加 1。
+ *
  * @ Copyright (c) Mistra,All Rights Reserved.
  * @ Github: https://github.com/MistraR
  * @ CSDN: https://blog.csdn.net/axela30w
