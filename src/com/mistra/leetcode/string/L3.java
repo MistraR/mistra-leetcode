352a2476acdb179bb4834e6377d14234ff3ea507
MistraR <842404548@qq.com>
2021-04-06 20:33:46 +0800

    3

---
+++ src/com/mistra/leetcode/string/L3.java
@@ -0,0 +1,36 @@
+package com.mistra.leetcode.string;
+
+
+import java.util.HashMap;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2021/3/31 下午10:32
+ * @ Description:
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+public class L3 {
+
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        System.out.println(a(s));
+    }
+
+    private static int a(String s) {
+        if (s.length() == 0) return 0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int max = 0;
+        int left = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                left = Math.max(left, map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            max = Math.max(max, i - left + 1);
+        }
+        return max;
+    }
+}
