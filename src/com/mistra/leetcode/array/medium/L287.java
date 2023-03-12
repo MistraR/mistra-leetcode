6ae333d128a48abc9de6ac6a21eabac29210c390
MistraR <842404548@qq.com>
2023-03-13 00:34:22 +0800

    L287

---
+++ src/com/mistra/leetcode/array/medium/L287.java
@@ -0,0 +1,56 @@
+package com.mistra.leetcode.array.medium;
+
+import java.util.HashSet;
+
+/**
+ * 给定一个包含n + 1 个整数的数组nums ，其数字都在[1, n]范围内（包括 1 和 n），可知至少存在一个重复的整数。
+ * <p>
+ * 假设 nums 只有 一个重复的整数 ，返回这个重复的数 。
+ * <p>
+ * 你设计的解决方案必须 不修改 数组 nums 且只用常量级 O(1) 的额外空间。
+ * <p>
+ *
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,3,4,2,2]
+ * 输出：2
+ *
+ * @author mistra@future.com
+ * @date 2023/3/11
+ */
+public class L287 {
+
+    public static void main(String[] args) {
+        System.out.println(findDuplicate(new int[]{1, 3, 4, 2, 2}));
+        System.out.println(findDuplicate2(new int[]{1, 3, 4, 2, 2}));
+    }
+
+    public static int findDuplicate2(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        int head = 0;
+        int tmp = fast;
+        while (head != tmp) {
+            head = nums[head];
+            tmp = nums[tmp];
+        }
+        return head;
+    }
+
+    public static int findDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return nums[i];
+            } else {
+                set.add(nums[i]);
+            }
+        }
+        return -1;
+    }
+}
