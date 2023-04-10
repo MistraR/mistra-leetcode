86605d33d09d3e3c06dc1ee3ab9d8ee9f8ea7264
MistraR <842404548@qq.com>
2023-04-10 23:39:06 +0800

    L98

--- src/com/mistra/leetcode/tree/ML98.java
+++ src/com/mistra/leetcode/tree/ML98.java
@@ -4,6 +4,13 @@ package com.mistra.leetcode.tree;
  * @author wrmistra@gmail.com
  * @date 2023/4/10
  * @ Description:
+ * 给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+ *
+ * 有效 二叉搜索树定义如下：
+ *
+ * 节点的左子树只包含 小于 当前节点的数。
+ * 节点的右子树只包含 大于 当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
  */
 public class ML98 {
 }
