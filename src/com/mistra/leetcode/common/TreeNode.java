47c610e74dbf94e806cc0fc7a33f04c8de57c71a
MistraR <842404548@qq.com>
2023-04-10 22:49:50 +0800

    TreeNode

---
+++ src/com/mistra/leetcode/common/TreeNode.java
@@ -0,0 +1,50 @@
+package com.mistra.leetcode.common;
+
+/**
+ * @author wrmistra@gmail.com
+ * @date 2023/4/10
+ * @ Description:
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+}
---
+++ src/com/mistra/leetcode/tree/EL94L144L145.java
@@ -0,0 +1,76 @@
+package com.mistra.leetcode.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mistra.leetcode.common.TreeNode;
+
+/**
+ * @author wrmistra@gmail.com
+ * @date 2023/4/10
+ * @ Description:给定一个二叉树的根节点 root ，返回 它的 前、中、后序 遍历，
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,3,2]
+ */
+public class EL94L144L145 {
+
+    /**
+     * 前序遍历
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        preorderRecursion(root, res);
+        return res;
+    }
+
+    public void preorderRecursion(TreeNode root, List<Integer> res) {
+        if (root != null) {
+            res.add(root.getVal());
+            postorderRecursion(root.getLeft(), res);
+            postorderRecursion(root.getRight(), res);
+        }
+    }
+
+    /**
+     * 中序遍历
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorderRecursion(root, res);
+        return res;
+    }
+
+    public void inorderRecursion(TreeNode root, List<Integer> res) {
+        if (root != null) {
+            inorderRecursion(root.getLeft(), res);
+            res.add(root.getVal());
+            inorderRecursion(root.getRight(), res);
+        }
+    }
+
+    /**
+     * 后序遍历
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        postorderRecursion(root, res);
+        return res;
+    }
+
+    public void postorderRecursion(TreeNode root, List<Integer> res) {
+        if (root != null) {
+            postorderRecursion(root.getLeft(), res);
+            postorderRecursion(root.getRight(), res);
+            res.add(root.getVal());
+        }
+    }
+}
