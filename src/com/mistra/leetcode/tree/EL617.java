90507393426573ab45d58b84c8b9114b0ecb4fbc
MistraR <842404548@qq.com>
2023-04-11 20:10:51 +0800

    EL617

---
+++ src/com/mistra/leetcode/tree/EL617.java
@@ -0,0 +1,36 @@
+package com.mistra.leetcode.tree;
+
+import com.mistra.leetcode.common.TreeNode;
+
+/**
+ * @author wrmistra@gmail.com
+ * @date 2023/4/11
+ * @ Description:
+ * 给你两棵二叉树： root1 和 root2 。
+ *
+ * 想象一下，当你将其中一棵覆盖到另一棵之上时，两棵树上的一些节点将会重叠（而另一些不会）。你需要将这两棵树合并成一棵新二叉树。
+ * 合并的规则是：如果两个节点重叠，那么将这两个节点的值相加作为合并后节点的新值；否则，不为 null 的节点将直接作为新二叉树的节点。
+ *
+ * 返回合并后的二叉树。
+ *
+ * 注意: 合并过程必须从两个树的根节点开始。
+ */
+public class EL617 {
+
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null || root2 == null) {
+            return root1 == null ? root2 : root1;
+        }
+        return dfs(root1, root2);
+    }
+
+    public TreeNode dfs(TreeNode root1, TreeNode root2) {
+        if (root1 == null || root2 == null) {
+            return root1 == null ? root2 : root1;
+        }
+        root1.setVal(root1.getVal() + root2.getVal());
+        root1.setLeft(dfs(root1.getLeft(), root2.getLeft()));
+        root1.setRight(dfs(root1.getRight(), root2.getRight()));
+        return root1;
+    }
+}
