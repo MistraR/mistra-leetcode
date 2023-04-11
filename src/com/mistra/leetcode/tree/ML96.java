e0218fc09019e65f399846a066483f3425459ebd
MistraR <842404548@qq.com>
2023-04-11 18:38:26 +0800

    ML96

---
+++ src/com/mistra/leetcode/tree/ML96.java
@@ -0,0 +1,16 @@
+package com.mistra.leetcode.tree;
+
+/**
+ * @author rui.wang
+ * @ Version: 1.0
+ * @ Time: 2023/4/11 17:33
+ * @ Description:给你一个整数 n ，求恰由 n 个节点组成且节点值从 1 到 n 互不相同的 二叉搜索树 有多少种？返回满足题意的二叉搜索树的种数
+ *
+ * 动态规划
+ */
+public class ML96 {
+
+    public int numTrees(int n) {
+
+    }
+}
