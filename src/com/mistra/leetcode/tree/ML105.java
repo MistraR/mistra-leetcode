df00e85c56b80ab76ce9b16728bc7d1f674986c8
MistraR <842404548@qq.com>
2023-04-12 23:48:09 +0800

    L105

---
+++ src/com/mistra/leetcode/tree/ML105.java
@@ -0,0 +1,18 @@
+package com.mistra.leetcode.tree;
+
+import com.mistra.leetcode.common.TreeNode;
+
+/**
+ * @author wrmistra@gmail.com
+ * @date 2023/4/12
+ * @ Description:
+ *
+ * 给定两个整数数组preorder 和 inorder，其中preorder 是二叉树的先序遍历， inorder是同一棵树的中序遍历，请构造二叉树并返回其根节点。
+ */
+public class ML105 {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+    }
+
+
+}
