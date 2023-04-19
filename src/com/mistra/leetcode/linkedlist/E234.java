d0047ea17e6aaf5e70146475f47ac3fcf422e9a8
MistraR <842404548@qq.com>
2023-04-19 22:56:41 +0800

    M234

--- src/com/mistra/leetcode/linkedlist/E234.java
+++ src/com/mistra/leetcode/linkedlist/E234.java
@@ -33,4 +33,30 @@ public class E234 {
         }
         return true;
     }
+
+    /**
+     * 双指针
+     */
+    public boolean isPalindrome2(ListNode head) {
+        List<Integer> vals = new ArrayList<Integer>();
+
+        // 将链表的值复制到数组中
+        ListNode currentNode = head;
+        while (currentNode != null) {
+            vals.add(currentNode.getVal());
+            currentNode = currentNode.getNext();
+        }
+
+        // 使用双指针判断是否回文
+        int front = 0;
+        int back = vals.size() - 1;
+        while (front < back) {
+            if (!vals.get(front).equals(vals.get(back))) {
+                return false;
+            }
+            front++;
+            back--;
+        }
+        return true;
+    }
 }
